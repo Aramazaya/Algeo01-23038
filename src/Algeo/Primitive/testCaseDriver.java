7cfa65fb749c8309296373014a8d9d0270cfee4c
hhansl <19623054@std.stei.itb.ac.id>
2024-10-21 15:47:37 +0700

    modified/chaned polinomial input

---
+++ src/Algeo/Primitive/testCaseDriver.java
@@ -0,0 +1,77 @@
+package Algeo.Primitive;
+import Algeo.Interpolance.PolinomialInterpolation;
+import Algeo.Primitive.InputOutput;
+import java.io.FileNotFoundException;
+
+
+public class testCaseDriver {
+    public static void main(String[] args) throws FileNotFoundException{
+        // SPL Augmented
+        // double[][] matrix = InputOutput.readMatrixFile("test/1a.txt");
+        // double[][] matrix = InputOutput.readMatrixFile("test/1b.txt");
+        // double[][] matrix = InputOutput.readMatrixFile("test/1c.txt");
+        // double[][] matrix = InputOutput.readMatrixFile("test/1d1.txt");
+        // double[][] matrix = InputOutput.readMatrixFile("test/1d2.txt");
+        // double[][] matrix = InputOutput.readMatrixFile("test/2a.txt");
+        // double[][] matrix = InputOutput.readMatrixFile("test/2b.txt");
+        // double[][] matrix = InputOutput.readMatrixFile("test/3a.txt");
+        // double[][] matrix = InputOutput.readMatrixFile("test/3b.txt");
+        // double[][] matrix = InputOutput.readMatrixFile("test/4.txt");
+
+        // Interpolasi Polinomial
+        // double[][] matrix = InputOutput.readMatrixFile("test/5a1.txt");
+        // double[][] matrix = InputOutput.readMatrixFile("test/5a2.txt");
+        // double[][] matrix = InputOutput.readMatrixFile("test/5a3.txt");
+        // double[][] matrix = InputOutput.readMatrixFile("test/5a4.txt");
+        // double[][] matrix = InputOutput.readMatrixFile("test/5b1.txt");
+        // double[][] matrix = InputOutput.readMatrixFile("test/5b2.txt");
+        // double[][] matrix = InputOutput.readMatrixFile("test/5b3.txt");
+        // double[][] matrix = InputOutput.readMatrixFile("test/5c.txt");
+
+        // *** DETERMINANT ***
+        // System.out.println("Row Deduction Determinant : " + Determinant.rowReductionDeterminant(matrix_n));
+        // BasicFunction.copyMatrix(matrix, matrix_n);
+        // System.out.println("Cofactor Expansion Determinant : " + CofactorExpansion.determinant(matrix_n));
+        // BasicFunction.copyMatrix(matrix, matrix_n);
+        
+        // *** INVERSE ***
+        // try{System.out.println("Inverse : ");BasicFunction.printMatrix(Inverse.InverseCofactor(matrix_n));}
+        // catch(IllegalArgumentException e){System.out.println(e.getMessage());}
+        // BasicFunction.copyMatrix(matrix, matrix_n);
+        // try{System.out.println("Inverse : ");BasicFunction.printMatrix(Inverse.InverseERO(matrix_n));}
+        // catch(IllegalArgumentException e){System.out.println(e.getMessage());}
+        
+        
+        // *** CRAMER ***
+        // BasicFunction.copyMatrix(matrix, matrix_n);
+        // System.out.println("Cramer Solution : ");BasicFunction.printArray(Cramer.CramerSolver(matrix));
+        
+        // *** GAUSS ***
+        // System.out.println("Gauss Solution : ");
+        // BasicFunction.copyMatrix(matrix, matrix_n);
+        // System.out.println("Matriks Awal: ");
+        // BasicFunction.printMatrix(matrix_n);
+        
+        // System.out.println("Matrix Awal:");
+        // BasicFunction.copyMatrix(matrix, matrix_n);
+        // BasicFunction.printMatrix(matrix_n);
+        // String result = GaussElimination.gaussElimination(matrix_n);
+        // System.out.println("Matrix Akhir:");
+        // BasicFunction.printMatrix(matrix_n);
+        // System.out.println(result);
+        
+        // System.out.println("Gauss-Jordan Solution: ");
+        // BasicFunction.copyMatrix(matrix, matrix_n);
+        // System.out.println("Matriks Awal: ");
+        // BasicFunction.printMatrix(matrix);
+        // result = GaussJordanElimination.gaussJordanElimination(matrix_n);
+        // System.out.println("Matrix Akhir:");
+        // BasicFunction.printMatrix(matrix_n);
+        // System.out.println(result);
+        
+        // *** INTERPOLATION *** 
+        // BasicFunction.copyMatrix(matrix, matrix_n);
+        // double result = PolinomialInterpolation.polinomialInterpolation(matrix);
+        // System.err.println("Polinomial Interpolation: " + result);
+    }
+}
